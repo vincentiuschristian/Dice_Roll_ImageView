344b8537cca6dca9eedab6ff007f5df730be7749
Vincentius Christian Chandra <crist.vincent10@gmail.com>
2023-08-02 11:32:58 +0700

    simple dice roll

---
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.diceroll
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.DiceBtn)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTv: TextView = findViewById(R.id.NumberTv)
+        resultTv.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
